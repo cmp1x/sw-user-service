05862dfb280e494bfc7d6adf463c19694cb95eb3
Vladimir Chirushin <motpfofs@gmail.com>
2022-05-05 01:50:10 +0300

    Added user model

---
+++ SW.UserService.Core/Models/User.cs
@@ -0,0 +1,34 @@
+﻿namespace SW.UserService.Core.Models
+{
+    using System;
+    using SW.UserService.Core.Enums;
+
+    class User
+    {
+        public string Id { get; set; }
+        
+        public int Cursor { get; set; }
+        
+        public DateTime CreatedAt { get; set; }
+        
+        public DateTime UpdatedAt { get; set; }
+        
+        public string Username { get; set; }
+        
+        public string Email { get; set; }
+        
+        public string EncryptedPassword { get; set; }
+        
+        public UserRole Role { get; set; }
+        
+        public string ConfirmationToken { get; set; }
+        
+        public DateTime ConfirmedAt { get; set; }
+        
+        public string ResetPasswordToken { get; set; }
+        
+        public DateTime ResetPasswordTokenSentAt { get; set; }
+        
+        public string AvatarUrl { get; set; }
+    }
+}
